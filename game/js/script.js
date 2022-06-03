a046f342479c9548e2250373d357bbbf4d116bb5
moppurin <98684224+moppurin@users.noreply.github.com>
2022-06-03 17:05:52 +0900

    canvas

--- game/js/script.js
+++ game/js/script.js
@@ -1,35 +1,68 @@
 
-      // 画面に画像を表示する
-      document.write( '<img id="rico" src="rico.png">' );
-
-      // キャラクターの位置
-      // var y = 0;
-      // var x = 0;
-
+      // canvasの設定
+      var canvas = document.getElementById('canvas');
+      // canvasの横幅
+      canvas.width = 640;
+      // canvasの縦幅
+      canvas.height = 640;
+      
+      // コンテキストを取得
+      var ctx = canvas.getContext('2d');
+      
       // りこちゃんのオブジェクトを作成
       var rico = new Object();
+      rico.img = new Image();
+      rico.img.src = 'img/rico.png';
       rico.x = 0;
       rico.y = 0;
       rico.move = 0;
-
-      // 押されたキーを入れておくための変数
-      var pressed_key = '';
-
+      
       // キーボードのオブジェクトを作成
       var key = new Object();
       key.up = false;
       key.down = false;
       key.right = false;
       key.left = false;
+      key.push = '';
 
+      // 画面に画像を表示する
+      // document.write( '<img id="rico" src="rico.png">' );
+      
+      // 画像のオブジェクトを作成
+      // var img = new Image();
+      // img.src = 'img/rico.png';
+      
+      // function main() {
+        
+      //   requestAnimationFrame(main);
+      // }
+      
+      // キャラクターの位置
+      // var y = 0;
+      // var x = 0;
+      
+      
+      // 押されたキーを入れておくための変数
+      // var pressed_key = '';
+      
+      
       // メインループ
       function main() {
-
+        
+        // 塗りつぶす色を指定
+        ctx.fillStyle = "rgb(0, 0, 0)";
+        // x=0, y=0 の位置に縦横30pxの正方形を描く
+        // 塗りつぶす
+        ctx.fillRect(0, 0, 640, 640);
+        
+        // 画像を表示
+        ctx.drawImage(rico.img, rico.x, rico.y);
+        
         // キーボードが押されたとき、keydownfunc関数を呼び出す
-        addEventListener( "keydown", keydownfunc );
+        addEventListener( "keydown", keydownfunc, false );
         // キーボードが離されたとき、keyupfunc関数を呼び出す
-        addEventListener( "keyup", keyupfunc );
-
+        addEventListener( "keyup", keyupfunc, false );
+        
         // rico.moveが0のとき、りこちゃんが移動する準備をする
         if( rico.move === 0) {
           if( key.left === true)  {
@@ -57,7 +90,7 @@
         // if( key.right === true ) rico.x += 32;
         // 「左ボタン」が押されたとき、yの値から32を足し算する
         // if( key.down === true ) rico.y += 32;
-
+        
         // rico.moveが0より大きいとき、りこちゃんが移動する
         if( rico.move > 0 ) {
           rico.move -= 4;
@@ -66,18 +99,21 @@
           if( pressed_key === 'right' ) rico.x += 4;
           if( pressed_key === 'down' ) rico.y += 4;
         }
-
+        
         // りこちゃんの画像の位置を反映させる
-        document.getElementById( 'rico' ).style.top = rico.y + "px";
-        document.getElementById( 'rico' ).style.left = rico.x + "px";
-
+        // document.getElementById( 'rico' ).style.top = rico.y + "px";
+        // document.getElementById( 'rico' ).style.left = rico.x + "px";
+        
         // main関数、つまり自分自身の関数を呼び出すことでループさせる
         requestAnimationFrame(main);
-
+        
       }
+      
+      // ページ依存している全てのデータが読み込まれたらメインループ開始
+      addEventListener('load', main(),false);
 
-      requestAnimationFrame(main);
-
+      // requestAnimationFrame(main);
+      
       // キーが押されたときに呼び出される関数
       function keydownfunc( event ) {
       // 押されたボタンに割り当てられた数値を、key_codeに代入
@@ -90,6 +126,7 @@
       if( key_code === 39 ) key.right = true;
       // 「左ボタン」が押されたとき、key.downをtrueにする
       if( key_code === 40 ) key.down = true;
+      event.preventDefault();
       }
 
       // キーが離されたときに呼び出される関数
