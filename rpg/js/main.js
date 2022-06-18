88c45f28dde7b638b240f3503752b31aa2f9fb83
moppurin <98684224+moppurin@users.noreply.github.com>
2022-06-18 14:13:36 +0900

    [add]キャラクターとマップの初期位置を調節しよう！

--- rpg/js/main.js
+++ rpg/js/main.js
@@ -19,6 +19,8 @@ addEventListener( 'load', () => {
     [11, 4, 4, 4, 4, 4, 4,11, 4,11],
     [11,11,11,11,11,11,11,11,11,11],
   ];
+  // タイルのサイズ
+  const TILE_SIZE = 32;
   // 歩く速さ
   const WALKING_SPEED = 4;
 
@@ -29,21 +31,26 @@ addEventListener( 'load', () => {
   const tilemap = new Tilemap( 'img/tile.png' );
   // tilemap.dataに、どんなマップなのか教える
   tilemap.data = map;
+  // マップ全体の位置をずらす
+  tilemap.x = TILE_SIZE*4 - TILE_SIZE/2;
+  tilemap.y = TILE_SIZE*3 - TILE_SIZE/2;
   // マップを登録する
   scene.add( tilemap );
 
   // 変数yamadaに、あなたは山田先生のタイルですよ、と教える
   const yamada = new Tile( 'img/yamada.png' );
+  // 山田先生を画面の中央を配置
+  yamada.x = yamada.y = TILE_SIZE*5 - TILE_SIZE/2;
   // tilemapに、山田先生のタイルを追加して、とお願いする
   tilemap.add( yamada );
 
   // ループから常に呼び出される
   scene.onenterframe = () => {
     // キーが押されたとき、山田先生が移動する
-    if ( game.input.left ) yamada.x -= WALKING_SPEED;
-    if ( game.input.right ) yamada.x += WALKING_SPEED;
-    if ( game.input.up ) yamada.y -= WALKING_SPEED;
-    if ( game.input.down ) yamada.y += WALKING_SPEED;
+    if ( game.input.left ) tilemap.x -= WALKING_SPEED;
+    if ( game.input.right ) tilemap.x += WALKING_SPEED;
+    if ( game.input.up ) tilemap.y -= WALKING_SPEED;
+    if ( game.input.down ) tilemap.y += WALKING_SPEED;
   } // yamada.onenterframe 終了
   // gameに、山田先生のスプライト画像を表示して、とお願いする
   // game.add( yamada );
